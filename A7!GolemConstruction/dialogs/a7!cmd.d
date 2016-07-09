111d9823276b6eff38a8e124e20a2cbda5a244ff
Argent77 <argent77@server.fake>
2016-07-09 20:09:48 +0200

    Fixed and updated golem command set; updated weapon of magic golems; updated apr of magic and lightning golems

--- A7!GolemConstruction/dialogs/a7!cmd.d
+++ A7!GolemConstruction/dialogs/a7!cmd.d
@@ -13,17 +13,17 @@ IF ~Allegiance(LastTalkedToBy, PC) NextTriggerObject(LastTalkedToBy) HaveSpellRE
       DO ~SetGlobal("Command", "LOCALS", 0)~ EXIT
   + ~!Global("Command", "LOCALS", 0) !Allegiance(Myself, FAMILIAR)~ + @45002 /* Follow me! */
       DO ~SetGlobal("Command", "LOCALS", 0) ChangeEnemyAlly(Myself, FAMILIAR) AddFamiliar()~ EXIT
-  + ~!Global("Command", "LOCALS", 1) !Name("a7!glmg", Myself) Allegiance(Myself, FAMILIAR)~ + @45003 /* Follow and protect me! */
+  + ~!Global("Command", "LOCALS", 1) Allegiance(Myself, FAMILIAR)~ + @45003 /* Follow and protect me! */
       DO ~SetGlobal("Command", "LOCALS", 1)~ EXIT
-  + ~!Global("Command", "LOCALS", 1) !Name("a7!glmg", Myself) !Allegiance(Myself, FAMILIAR)~ + @45003 /* Follow and protect me! */
+  + ~!Global("Command", "LOCALS", 1) !Allegiance(Myself, FAMILIAR)~ + @45003 /* Follow and protect me! */
       DO ~SetGlobal("Command", "LOCALS", 1) ChangeEnemyAlly(Myself, FAMILIAR) AddFamiliar()~ EXIT
   + ~!Global("Command", "LOCALS", 2) Allegiance(Myself, NEUTRAL)~ + @45004 /* Stand guard! */
       DO ~SetGlobal("Command", "LOCALS", 2) SaveObjectLocation("LOCALS", "Location", Myself) Face(S)~ EXIT
   + ~!Global("Command", "LOCALS", 2) !Allegiance(Myself, NEUTRAL)~ + @45004 /* Stand guard! */
       DO ~SetGlobal("Command", "LOCALS", 2) RemoveFamiliar() ChangeEnemyAlly(Myself, GOODBUTBLUE) SaveObjectLocation("LOCALS", "Location", Myself) Face(S)~ EXIT
-  + ~!Global("Command", "LOCALS", 2) !Name("a7!glmg", Myself) Allegiance(Myself, NEUTRAL)~ + @45005 /* Stand guard and defend the area! */
+  + ~!Global("Command", "LOCALS", 2) Allegiance(Myself, NEUTRAL)~ + @45005 /* Stand guard and defend the area! */
       DO ~SetGlobal("Command", "LOCALS", 3) SaveObjectLocation("LOCALS", "Location", Myself) Face(S)~ EXIT
-  + ~!Global("Command", "LOCALS", 2) !Name("a7!glmg", Myself) !Allegiance(Myself, NEUTRAL)~ + @45005 /* Stand guard and defend the area! */
+  + ~!Global("Command", "LOCALS", 2) !Allegiance(Myself, NEUTRAL)~ + @45005 /* Stand guard and defend the area! */
       DO ~SetGlobal("Command", "LOCALS", 3) RemoveFamiliar() ChangeEnemyAlly(Myself, GOODBUTBLUE) SaveObjectLocation("LOCALS", "Location", Myself) Face(S)~ EXIT
   ++ @45006 /* Destroy yourself! */ + Golem.3
   ++ @45007 /* Nevermind. */ EXIT
