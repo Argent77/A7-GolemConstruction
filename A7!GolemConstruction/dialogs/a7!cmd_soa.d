f9593f727f56764b5b25ccc8faf90b0b59e73a1c
Argent77 <argent77@server.fake>
2016-07-10 16:34:51 +0200

    Handling specific SoD and SoA plot-related events; added penalties to golem when damaged by 75% or more

---
+++ A7!GolemConstruction/dialogs/a7!cmd_soa.d
@@ -0,0 +1,9 @@
+// *** Golem command - Special ***
+
+APPEND ~a7!cmd~
+  // Special: SoA Spellhold
+  IF WEIGHT #-1 ~Global("spellhold", "LOCALS", 1)~ Golem.Spellhold
+    SAY @45011 /* The golem has been rendered inactive by the protective magics of this place. */
+    IF ~~ EXIT
+  END
+END
