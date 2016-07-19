b69f38894258135d9a3bdb83459f826d60d334c3
Argent77 <argent77@server.fake>
2016-07-19 12:36:40 +0200

    Fixed conditions for Jerlia the Ore Merchant

--- A7!GolemConstruction/dialogs/scjerlia.d
+++ A7!GolemConstruction/dialogs/scjerlia.d
@@ -12,19 +12,19 @@ APPEND ~scjerlia~
 
   IF WEIGHT #4 ~Global("A7!GolemAbility", "GLOBAL", 1) !Global("A7!Intro", "MYAREA", 0)~ jerlia.intro.2
     SAY @44501 /* Ah, the golem maker returns. Do you need another chunk of ore? */
-    + ~!Global("A7!UpdateStore", "LOCALS", 1)~ + @44502 /* Yes, I'd like to browse your store again. */ + jerlia.store.2
-    + ~Global("A7!UpdateStore", "LOCALS", 1)~ + @44502 /* Yes, I'd like to browse your store again. */ DO ~SetGlobal("A7!UpdateStore", "LOCALS", 2)~ + jerlia.store.3
+    + ~!Global("A7!UpdateStore", "MYAREA", 1)~ + @44502 /* Yes, I'd like to browse your store again. */ + jerlia.store.2
+    + ~Global("A7!UpdateStore", "MYAREA", 1)~ + @44502 /* Yes, I'd like to browse your store again. */ DO ~SetGlobal("A7!UpdateStore", "MYAREA", 2)~ + jerlia.store.3
     ++ @44503 /* Not at the moment. Maybe later. */ + jerlia.bye.1
   END
   
   IF ~~ jerlia.1
     SAY @44504 /* You kiddin' me, right? Well, I usually sell in bulk, but I might do an exception in your case. What you lookin' for? */
-    + ~!Global("A7!UpdateStore", "LOCALS", 1)~ + @44505 /* I'm interested in clay or stone. */ + jerlia.store.1
-    + ~Global("A7!UpdateStore", "LOCALS", 1)~ + @44505 /* I'm interested in clay or stone. */ DO ~SetGlobal("A7!UpdateStore", "LOCALS", 2)~ + jerlia.store.1
-    + ~!Global("A7!UpdateStore", "LOCALS", 1) OR(3) Global("A7!TomeGolemIron", "GLOBAL", 1) Global("A7!TomeGolemMithral", "GLOBAL", 1) Global("A7!TomeGolemIce", "GLOBAL", 1)~ 
+    + ~!Global("A7!UpdateStore", "MYAREA", 1)~ + @44505 /* I'm interested in clay or stone. */ + jerlia.store.1
+    + ~Global("A7!UpdateStore", "MYAREA", 1)~ + @44505 /* I'm interested in clay or stone. */ DO ~SetGlobal("A7!UpdateStore", "MYAREA", 2)~ + jerlia.store.1
+    + ~!Global("A7!UpdateStore", "MYAREA", 1) OR(3) Global("A7!TomeGolemIron", "GLOBAL", 1) Global("A7!TomeGolemMithral", "GLOBAL", 1) Global("A7!TomeGolemIce", "GLOBAL", 1)~ 
         + @44506 /* Do you have something special on sale? Maybe iron, or even mithral? */ + jerlia.store.1
-    + ~Global("A7!UpdateStore", "LOCALS", 1) OR(3) Global("A7!TomeGolemIron", "GLOBAL", 1) Global("A7!TomeGolemMithral", "GLOBAL", 1) Global("A7!TomeGolemIce", "GLOBAL", 1)~ 
-        + @44506 /* Do you have something special on sale? Maybe iron, or even mithral? */ DO ~SetGlobal("A7!UpdateStore", "LOCALS", 2)~ + jerlia.store.1
+    + ~Global("A7!UpdateStore", "MYAREA", 1) OR(3) Global("A7!TomeGolemIron", "GLOBAL", 1) Global("A7!TomeGolemMithral", "GLOBAL", 1) Global("A7!TomeGolemIce", "GLOBAL", 1)~ 
+        + @44506 /* Do you have something special on sale? Maybe iron, or even mithral? */ DO ~SetGlobal("A7!UpdateStore", "MYAREA", 2)~ + jerlia.store.1
   END
 
   IF ~~ jerlia.store.1
