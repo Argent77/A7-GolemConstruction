ddfad483324ded36ce47e7327d268185a78db3cd
Argent77 <argent77@server.fake>
2017-01-22 00:08:16 +0100

    Added Limited Wish option to turn iron into mithral (IWD:EE)

--- A7!GolemConstruction/dialogs/iwdee/wishes.d
+++ A7!GolemConstruction/dialogs/iwdee/wishes.d
@@ -1,5 +1,28 @@
 // *** Adding new replies to Wish dialog (IWDEE) ***
 
+// Limited Wish
+EXTEND_BOTTOM ~wish~ 1
+  + ~Global("A7!TomeGolemMithral", "GLOBAL", 1) PartyHasItem("a7!iron") CheckStatLT(LastTalkedToBy, 14, WIS) Global("a7!wishMithral", "GLOBAL", 0)~ + 
+      @41506 /* I wish for my iron to be turned into mithral. */ DO ~SetGlobal("a7!wishMithral", "GLOBAL", 1)~ + Limited.Mithral.Low
+  + ~Global("A7!TomeGolemMithral", "GLOBAL", 1) PartyHasItem("a7!iron") CheckStatGT(LastTalkedToBy, 13, WIS) Global("a7!wishMithral", "GLOBAL", 0)~ + 
+      @41507 /* I wish for a single piece of my iron to be turned into mithral. */ DO ~SetGlobal("a7!wishMithral", "GLOBAL", 1)~ + Limited.Mithral.High
+END
+
+APPEND ~wish~
+  IF ~~ Limited.Mithral.Low
+    SAY @41508 /* It is done, master. All your iron has been turned into a single piece of mithral. */
+    IF ~~ DO ~TakePartyItemAll("a7!iron") DestroyItem("a7!iron") GiveItemCreate("a7!mith", LastTalkedToBy, 1, 0, 0)
+              SetInterrupt(FALSE) ForceSpell(Myself, DRYAD_TELEPORT)~ EXIT
+  END
+
+  IF ~~ Limited.Mithral.High
+    SAY @41509 /* It is done, master. One iron bar in your possession has been turned into a single piece of mithral. */
+    IF ~~ DO ~TakePartyItemNum("a7!iron", 1) DestroyItem("a7!iron") GiveItemCreate("a7!mith", LastTalkedToBy, 1, 0, 0)
+              SetInterrupt(FALSE) ForceSpell(Myself, DRYAD_TELEPORT)~ EXIT
+  END
+END
+
+
 // Wish
 EXTEND_BOTTOM ~wish25~ 12
   + ~Global("A7!TomeGolemMithral", "GLOBAL", 1) PartyHasItem("a7!iron") CheckStatLT(LastTalkedToBy, 14, WIS)~ + @41510 /* Turn all your iron into a single piece of mithral. */
