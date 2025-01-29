97cdec7a13e3df62e93ca909a302c86b794566a6
Argent77 <4519923+Argent77@users.noreply.github.com>
2025-01-29 11:32:21 +0100

    Fix potential compatibility issue in Jerlia's dialog
    Fixes #13

--- A7-GolemConstruction/dialogs/scjerlia_bye_response.d
+++ A7-GolemConstruction/dialogs/scjerlia_bye_response.d
@@ -3,5 +3,12 @@
 // Should only be added once
 ADD_TRANS_TRIGGER ~scjerlia~ 37 ~False()~ DO 0 UNLESS ~"False()"~
 EXTEND_TOP ~scjerlia~ 37 #1
-  ++ #28743 /* Nothing, sorry I bothered you. */ + 2
+  ++ #28743 /* Nothing, sorry I bothered you. */ + scjerlia.bye
+END
+
+APPEND ~scjerlia~
+  IF ~~ scjerlia.bye
+    SAY #28746 /* S'all right friend. Come back when you need. */
+    IF ~~ EXIT
+  END
 END
